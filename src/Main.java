fe00778700c60acc1db7389bfb4634c1e5867430
redian23 <vasya.degski@gmail.com>
2019-10-20 13:38:50 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("!!");
+        System.out.print("Hi");
+        System.out.print (" I'm");
+        System.out.println(" Redian ");
+        System.out.println("!!");
+        System.out.println("1");
+        System.out.println("23");
+        System.out.println("456");
+    }
+}
