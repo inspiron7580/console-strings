ed3803a0d546001b490fbbb1fb7d07faef7118cb
Вася Климов <46902231+redian23@users.noreply.github.com>
2019-10-24 20:53:43 +0300

    Delete some strings

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,5 @@ public class Main {
         System.out.println(" Redian ");
         System.out.println("!!");
         System.out.println("1");
-        System.out.println("23");
-        System.out.println("456");
     }
 }
